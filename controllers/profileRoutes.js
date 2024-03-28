adc8a0f899c234fe81e37cdb97bbcfd289df064d
livefromthelevel <brian.d.wach@gmail.com>
2024-03-27 20:25:55 -0500

    <No changes>

--- controllers/profileRoutes.js
+++ controllers/profileRoutes.js
@@ -5,7 +5,7 @@ const withAuth = require('../utils/auth');
 // Default profile route showing most recently created recipes
 router.get('/', withAuth, async (req, res) => {
     try {
-      // Get 15 most recently post recipes by the community
+      // Get all recipes created by logged in user
       const recipesData = await Recipes.findAll({
         attributes: ['id', 'title'],
         include: [{ model: Users, attributes: ['id'] }],
@@ -33,6 +33,29 @@ router.get('/', withAuth, async (req, res) => {
       const recipesData = await Liked_recipes.findAll({
         attributes: ['id', 'title'],
         include: [{ model: Recipes, attributes: ['id', 'title', 'users_id'] }],
+        include: [{ model: Users, attributes: ['username'] }],
+        order: [['date_created', 'DESC']],
+        where: { user_id: req.session.user_id }
+      });
+  
+      // Serialize data so the template can read it
+      const recipes = recipesData.map((post) => post.get({ plain: true }));
+  
+      // Pass serialized data and session flag into template
+      res.render('liked', {
+        recipes,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  router.get('/list', withAuth, async (req, res) => {
+    try {
+      // Get 15 most recently post recipes by the community
+      const recipesData = await Shopping_list.findAll({
+        include: [{ model: Recipes, attributes: ['id', 'title'] }],
         include: [{ model: Users, attributes: ['username'] }],
         order: [['date_created', 'DESC']],
         where: { user_id: req.session.user_id }
