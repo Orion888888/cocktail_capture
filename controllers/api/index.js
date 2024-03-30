5cb295f717fe712248069a110e1e7442bdc06ec9
livefromthelevel <brian.d.wach@gmail.com>
2024-03-29 19:06:34 -0500

    <Added API route for seeding the remote database>

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const usersRoutes = require('./usersRoutes');
 const recipeRoutes = require('./recipeRoutes.js');
+const seedRoutes = require('./seedRoutes.js');
 
 router.use('/users', usersRoutes);
 router.use('/recipes', recipeRoutes);
+router.use('/seed', seedRoutes);
+
 module.exports = router;
