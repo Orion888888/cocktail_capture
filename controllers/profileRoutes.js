02e1a19c7da3084701f3eaf13503eaab689025fb
livefromthelevel <brian.d.wach@gmail.com>
2024-03-29 19:13:45 -0500

    <Adjust profileRoutes.js to render notice if a user had not create any recipes yet>

--- controllers/profileRoutes.js
+++ controllers/profileRoutes.js
@@ -18,6 +18,7 @@ router.get('/', withAuth, async (req, res) => {
 
       res.render('profile', {
         recipes,
+        none: (recipes.length === 0),
         logged_in: req.session.logged_in
       });
     } catch (err) {
