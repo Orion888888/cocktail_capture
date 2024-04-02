d10fac5222463b8b4f09002a7731f0595d333eb2
Pedro Gonzalez <orion@Pedros-MacBook-Pro.local>
2024-04-01 20:09:04 -0500

    adding the dislike button logic for the possibility of future development.

--- public/js/like.js
+++ public/js/like.js
@@ -7,4 +7,12 @@ likeBtn.addEventListener('click', function () {
         likeBtn.classList.toggle('transform');
         likeBtn.classList.toggle('scale-110');
         likeBtn.classList.toggle('rotate-180');
-        likeBtn.innerHTML = likeBtn.classList
+        likeBtn.innerHTML = likeBtn.classList                                     .contains('bg-green-500') ? 
+        '<i class="fas fa-thumbs-down text-white text-xl"></i>' 
+      : '<i class="fas fa-thumbs-up text-gray-500 text-xl"></i>';
+      dislikeBtn.classList
+                  .remove('bg-red-500', 'text-white', 
+                        'transform', 'scale-110', 'rotate-180');
+      dislikeBtn.innerHTML = 
+        '<i class="fas fa-thumbs-down text-gray-500 text-xl"></i>';
+  });
