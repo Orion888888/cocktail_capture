8b5355dd16519338212955ce87f991fbdd6765c9
Pedro Gonzalez <orion@Pedros-MacBook-Pro.local>
2024-04-01 20:27:43 -0500

    adding the attribute names to the button that will be used.

--- public/js/like.js
+++ public/js/like.js
@@ -2,4 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
     // Select all elements with class 'like-btn'
     const likeButtons = document.querySelectorAll('.like-btn');
     
-    // Add event listener to each like button
+    // Add event listener to like button
+    likeButtons.forEach(button => {
+        button.addEventListener('click', async () => {
+            // Retrieve drink ID from data attribute
+            const drinkId = button.getAttribute('data-drink-id');
+            //Perform an action when the button is clicked (e.g., change button color)
+            button.classList.toggle('liked');
