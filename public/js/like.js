09fb6717eebf2ad3238da323115a883729061eb8
Pedro Gonzalez <orion@Pedros-MacBook-Pro.local>
2024-04-01 20:10:38 -0500

    adding the same functionailty 'toggle' to the dislike button.

--- public/js/like.js
+++ public/js/like.js
@@ -16,3 +16,19 @@ likeBtn.addEventListener('click', function () {
       dislikeBtn.innerHTML = 
         '<i class="fas fa-thumbs-down text-gray-500 text-xl"></i>';
   });
+  dislikeBtn.addEventListener('click', function () {
+    dislikeBtn.classList.toggle('bg-red-500');
+    dislikeBtn.classList.toggle('text-white');
+    dislikeBtn.classList.toggle('transform');
+    dislikeBtn.classList.toggle('scale-110');
+    dislikeBtn.classList.toggle('rotate-180');
+    dislikeBtn.innerHTML = dislikeBtn.classList
+                                        .contains('bg-red-500') ?
+      '<i class="fas fa-thumbs-up text-white text-xl"></i>' :
+      '<i class="fas fa-thumbs-down text-gray-500 text-xl"></i>';
+    likeBtn.classList
+             .remove('bg-green-500', 'text-white', 'transform',
+                   'scale-110', 'rotate-180');
+    likeBtn.innerHTML = 
+      '<i class="fas fa-thumbs-up text-gray-500 text-xl"></i>';
+});
