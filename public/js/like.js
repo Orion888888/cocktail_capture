d0f59709d4c78e9b3527c9cf9a8bc877ee44f650
Pedro Gonzalez <orion@Pedros-MacBook-Pro.local>
2024-04-01 20:07:42 -0500

    adding the toggle click functions with the addEventListener so that the button has movement on click.

--- public/js/like.js
+++ public/js/like.js
@@ -1,3 +1,10 @@
 let likeBtn = document.querySelector('.like-btn');
 let dislikeBtn = document.querySelector('.dislike-btn');
-        likeBtn.addEventListener('click', function () {
+
+likeBtn.addEventListener('click', function () {             
+        likeBtn.classList.toggle('bg-green-500');
+        likeBtn.classList.toggle('text-white');
+        likeBtn.classList.toggle('transform');
+        likeBtn.classList.toggle('scale-110');
+        likeBtn.classList.toggle('rotate-180');
+        likeBtn.innerHTML = likeBtn.classList
