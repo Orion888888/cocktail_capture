b4ee0b4d47b431c225fca1ac18fa546e6c2cdbfd
livefromthelevel <brian.d.wach@gmail.com>
2024-04-04 00:30:35 -0500

    <Logic to view and add recipes to cart is working.  Working on adjusting button classes dynamically>

--- controllers/api/cartRoutes.js
+++ controllers/api/cartRoutes.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { Users, Recipes, Liked_recipes, Ingredients, Recipes_ingredients, Shopping_list_recipes, Shopping_list } = require('../../models');
 const withAuth = require('../../utils/auth');
-const { data } = require('autoprefixer');
-
 
 
 //Route to add item to cart
@@ -24,7 +22,23 @@ router.post('/:id', withAuth, async (req, res) => {
 
 });
 
-//Make a remove 
 
+//Route to remove item from cart
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const response = await Shopping_list.findOne({where: {user_id: req.session.user_id}});
+        const data = response.get({ plain: true });
+       
+        const cartId = data.id;
+        const cartItem = await Shopping_list_recipes.destroy({ where: { recipes_id: req.params.id, shopping_list_id: cartId}});
+
+        //success response
+        res.status(200).json(cartItem);
+    } catch (error) {
+        console.error('Error adding item to cart:', error);
+        res.sendStatus(500);
+    };
+
+});
 
 module.exports = router;
--- controllers/profileRoutes.js
+++ controllers/profileRoutes.js
@@ -5,31 +5,41 @@ const withAuth = require('../utils/auth');
 //Adding to Bar via my cart
 router.get("/cart", withAuth, async (req, res) => {
   try {
-    console.log('hello');
-      // Get all recipes created by logged in user
-      const response = await Shopping_list.findOne({where: {user_id: req.session.user_id}});
-      const data = response.get({ plain: true });
-      console.log(data);
-      const cartId = data.id;
+    // Get all recipes created by logged in user
+    const response = await Shopping_list.findOne({ where: { user_id: req.session.user_id } });
+    const data = response.get({ plain: true });
+    console.log(data);
+    const cartId = data.id;
 
-      const recipesData = await Shopping_list_recipes.findAll({
-        // attributes: ['id', 'strDrink', 'strDrinkThumb', 'description', 'date_created'],
-        include: Recipes,
-        //  include: [{ model: Liked_recipes, attributes: ['star_value'] }],
-        order: [['id', 'DESC']],
-        where: { shopping_list_id: cartId }
-         });
-    
-        // Pass serialized data and session flag into template
-        const recipes = recipesData.map((recipe) => recipe.get({ plain: true }));
-         console.log(recipes);
-        // for (list of recipes) {
-        //   console.log(list.ingredients);
-        // };
-        
-      // res.render('cart');
+    const recipesData = await Recipes.findAll({
+      include: [
+        {
+          model: Shopping_list,
+          through: {
+            model: Shopping_list_recipes
+          },
+          where: {
+            id: data.id // Add your specific filter condition here
+          }
+        }, Ingredients
+      ],
+      order: [['strDrink', 'ASC']],
+    });
+    // Pass serialized data and session flag into template
+    const recipes = recipesData.map((recipe) => recipe.get({ plain: true }));
+    console.log('RECIPE DATA');
+    console.log(recipes);
+    // for (list of recipes) {
+    //   console.log(list.ingredients);
+    // };
+
+    res.render('cart', {
+      recipes,
+      none: (recipes.length === 0),
+      logged_in: req.session.logged_in
+    });
   } catch (err) {
-      res.status(500).json(err);
+    res.status(500).json(err);
   }
 
 });
@@ -39,40 +49,40 @@ router.get("/cart", withAuth, async (req, res) => {
 
 // Default profile route showing most recently created recipes
 router.get('/', withAuth, async (req, res) => {
-    try {
-      // Get all recipes created by logged in user
-      const recipesData = await Recipes.findAll({
+  try {
+    // Get all recipes created by logged in user
+    const recipesData = await Recipes.findAll({
       // attributes: ['id', 'strDrink', 'strDrinkThumb', 'description', 'date_created'],
       include: Ingredients,
       //  include: [{ model: Liked_recipes, attributes: ['star_value'] }],
       order: [['date_created', 'DESC']],
       where: { user_id: req.session.user_id }
-       });
-  
-      // Pass serialized data and session flag into template
-      const recipes = recipesData.map((recipe) => recipe.get({ plain: true }));
+    });
 
-      for (list of recipes) {
-        console.log(list.ingredients);
-      };
-      
-      res.render('profile', {
-        recipes,
-        none: (recipes.length === 0),
-        deleted: (req.query.deleted),
-        logged_in: req.session.logged_in
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+    // Pass serialized data and session flag into template
+    const recipes = recipesData.map((recipe) => recipe.get({ plain: true }));
 
-// Route to render the create recipe form
-  router.get('/create', withAuth, (req, res) => {
-    res.render('create', {
+    for (list of recipes) {
+      console.log(list.ingredients);
+    };
+
+    res.render('profile', {
+      recipes,
+      none: (recipes.length === 0),
+      deleted: (req.query.deleted),
       logged_in: req.session.logged_in
     });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Route to render the create recipe form
+router.get('/create', withAuth, (req, res) => {
+  res.render('create', {
+    logged_in: req.session.logged_in
   });
+});
 
 // Route to render update/delete page for specific recipe
 router.get('/update/:id', withAuth, async (req, res) => {
@@ -82,7 +92,7 @@ router.get('/update/:id', withAuth, async (req, res) => {
     const recipeData = await Recipes.findByPk(req.params.id, {
       include: [Ingredients],
       attributes: ['id', 'strDrink', 'strDrinkThumb', 'description', 'date_created'],
-     });
+    });
 
     // Pass serialized data and session flag into template
     const recipe = recipeData.get({ plain: true });
@@ -99,54 +109,54 @@ router.get('/update/:id', withAuth, async (req, res) => {
   }
 });
 
-  /*
-  router.get('/liked', withAuth, async (req, res) => {
-    try {
-      // Get 15 most recently post recipes by the community
-      const recipesData = await Liked_recipes.findAll({
-        include: [{ model: Recipes, attributes: ['id', 'name', 'user_id', 'date_created'] }],
-        include: [{ model: Users, attributes: ['username'] }],
-        order: [['date_created', 'DESC']],
-        where: { user_id: req.session.user_id }
-      });
-  
-      // Serialize data so the template can read it
-      const recipes = recipesData.map((post) => post.get({ plain: true }));
-  
-      // Pass serialized data and session flag into template
-      res.render('liked', {
-        recipes,
-        logged_in: req.session.logged_in
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-  */
+/*
+router.get('/liked', withAuth, async (req, res) => {
+  try {
+    // Get 15 most recently post recipes by the community
+    const recipesData = await Liked_recipes.findAll({
+      include: [{ model: Recipes, attributes: ['id', 'name', 'user_id', 'date_created'] }],
+      include: [{ model: Users, attributes: ['username'] }],
+      order: [['date_created', 'DESC']],
+      where: { user_id: req.session.user_id }
+    });
+ 
+    // Serialize data so the template can read it
+    const recipes = recipesData.map((post) => post.get({ plain: true }));
+ 
+    // Pass serialized data and session flag into template
+    res.render('liked', {
+      recipes,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+*/
 
-  /*
-  router.get('/list', withAuth, async (req, res) => {
-    try {
-      // Get 15 most recently post recipes by the community
-      const recipesData = await Shopping_list.findAll({
-        include: [{ model: Recipes, attributes: ['id', 'name'] }],
-        include: [{ model: Users, attributes: ['username'] }],
-        order: [['date_created', 'DESC']],
-        where: { user_id: req.session.user_id }
-      });
-  
-      // Serialize data so the template can read it
-      const recipes = recipesData.map((post) => post.get({ plain: true }));
-  
-      // Pass serialized data and session flag into template
-      res.render('liked', {
-        recipes,
-        logged_in: req.session.logged_in
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  }); 
-  */
+/*
+router.get('/list', withAuth, async (req, res) => {
+  try {
+    // Get 15 most recently post recipes by the community
+    const recipesData = await Shopping_list.findAll({
+      include: [{ model: Recipes, attributes: ['id', 'name'] }],
+      include: [{ model: Users, attributes: ['username'] }],
+      order: [['date_created', 'DESC']],
+      where: { user_id: req.session.user_id }
+    });
+ 
+    // Serialize data so the template can read it
+    const recipes = recipesData.map((post) => post.get({ plain: true }));
+ 
+    // Pass serialized data and session flag into template
+    res.render('liked', {
+      recipes,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}); 
+*/
 
 module.exports = router;
