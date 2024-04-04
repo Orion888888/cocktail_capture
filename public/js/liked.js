53e304c47862128ad60b1628cc2fffac505edcd1
Pedro Gonzalez <orion@Pedros-MacBook-Pro.local>
2024-04-04 00:17:40 -0500

    created logic for the liked.js script file to like cocktail cards.

--- public/js/liked.js
+++ public/js/liked.js
@@ -13,35 +13,48 @@
 //     return card;
 // }
 
+const likedBtnArray = document.querySelectorAll('.like-btn');
 
-const likeHandler = async (event) => {
-    try {
-        const { target } = event;
-        const drinkId = target.getAttribute('data-id');
+const likeHandler = (cockTailRecipeIdOnParam) => {
+    likedBtnArray[cockTailRecipeIdOnParam].setAttribute("style", "display: none;");
 
-        // Send a POST request to add the liked recipe
-        const response = await fetch(`/api/recipes/liked/${drinkId}`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-        });
+    // TODO : We can add the remaining logic here to send the liked cocktail reciepe id
+    // into the database of liked recipes via an api.
+}
 
-        if (!response.ok) {
-            throw new Error('Failed to add recipe to liked');
-        }
+for (let cockTailRecipeIdOn = 0; cockTailRecipeIdOn < likedBtnArray.length; cockTailRecipeIdOn++) {
+    likedBtnArray[cockTailRecipeIdOn].addEventListener('click', () => likeHandler(cockTailRecipeIdOn));
+}
 
-        // Create the recipe card element; Append the card to the container
-        const recipeCard = createRecipeCard(drinkId);
-        document.querySelector('.liked-recipes').appendChild(recipeCard);
 
-        console.log('Recipe added to liked successfully');
-    } catch (error) {
-        console.error('Error occurred while liking recipe:', error);
-    }
-};
+// const likeHandler = async (event) => {
+//     try {
+//         const { target } = event;
+//         const drinkId = target.getAttribute('data-id');
 
-document.querySelectorAll('.like-btn').forEach(btn => {
-    btn.addEventListener('click', likeHandler);
-});
+//         // Send a POST request to add the liked recipe
+//         const response = await fetch(`/api/recipes/liked/${drinkId}`, {
+//             method: 'POST',
+//             headers: { 'Content-Type': 'application/json' },
+//         });
+
+//         if (!response.ok) {
+//             throw new Error('Failed to add recipe to liked');
+//         }
+
+//         // Create the recipe card element; Append the card to the container
+//         const recipeCard = createRecipeCard(drinkId);
+//         document.querySelector('.liked-recipes').appendChild(recipeCard);
+
+//         console.log('Recipe added to liked successfully');
+//     } catch (error) {
+//         console.error('Error occurred while liking recipe:', error);
+//     }
+// };
+
+// document.querySelectorAll('.like-btn').forEach(btn => {
+//     btn.addEventListener('click', likeHandler);
+// });
 
 
 
