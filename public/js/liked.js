873433909ca982fb84d8523f471783f80244261c
Snalepa <snalepa1998@gmail.com>
2024-04-03 23:40:27 -0400

    Like button disappears when clicked

--- public/js/liked.js
+++ public/js/liked.js
@@ -1,7 +1,13 @@
-const likeHandler = (event) => {
-    const { target } = event
-    const drinkId = target.getAttribute('')
-}
-document.querySelector('.like-btn').addEventListener('click', likeHandler);
 
-target.getAttribute("data-id")
+
+
+
+const likedBtnArray = document.querySelectorAll('.like-btn');
+
+const likeHandler = (cockTailRecipeIdOnParam) => {
+    likedBtnArray[cockTailRecipeIdOnParam].setAttribute("style", "display: none;");
+}
+
+for (let cockTailRecipeIdOn = 0; cockTailRecipeIdOn < likedBtnArray.length; cockTailRecipeIdOn++) {
+    likedBtnArray[cockTailRecipeIdOn].addEventListener('click', () => likeHandler(cockTailRecipeIdOn));
+}
