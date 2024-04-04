ba31c61e06f37f899c84022f27cd762e3b9451a0
Snalepa <snalepa1998@gmail.com>
2024-04-03 23:46:45 -0400

    Added notes regarding liked button

--- public/js/liked.js
+++ public/js/liked.js
@@ -1,11 +1,10 @@
-
-
-
-
 const likedBtnArray = document.querySelectorAll('.like-btn');
 
 const likeHandler = (cockTailRecipeIdOnParam) => {
     likedBtnArray[cockTailRecipeIdOnParam].setAttribute("style", "display: none;");
+
+    // TODO : We can add the remaining logic here to send the liked cocktail reciepe id
+    // into the database of liked recipes via an api.
 }
 
 for (let cockTailRecipeIdOn = 0; cockTailRecipeIdOn < likedBtnArray.length; cockTailRecipeIdOn++) {
